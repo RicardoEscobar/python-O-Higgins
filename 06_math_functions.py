b22cbfe332d4d31c087e5bbd35ffffad7c2df451
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-08-25 17:44:43 -0600

    feat: Add math functions examples in Python

---
+++ 06_math_functions.py
@@ -0,0 +1,20 @@
+"""Math functions in Python"""
+
+# Arithmetic in Python
+print(15 + 5)  # 20
+print(15 - 5)  # 10
+print(15 * 5)  # 75
+print(15 / 5)  # 3.0
+print(15 // 5)  # 3
+print(15 % 5)  # 0
+print(15 ** 5)  # 759375
+
+# Math functions in Python
+print(abs(-5))  # 5
+print(round(5.5))  # 6
+print(round(5.4))  # 5
+print(max(5, 10, 8))  # 10
+print(min(5, 10, 8))  # 5
+print(pow(5, 5))  # 3125
+print(sum([5, 10, 8]))  # 23
+print(divmod(5, 2))  # (2, 1)
