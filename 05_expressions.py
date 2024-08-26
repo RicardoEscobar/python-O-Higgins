d442e73d76d33d3815a5cf51ad5aade688ab3d7f
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-08-25 18:53:43 -0600

    feat: Add expressions examples in Python

---
+++ 05_expressions.py
@@ -0,0 +1,21 @@
+"""
+Expressions are the building blocks of Python.
+They are the smallest
+"""
+
+# Arithmetic Expressions
+5 + 3 * 2 
+
+# Variable Expressions
+x = 10
+y = 5
+result = x - y
+
+# Boolean Expressions
+7 > 3
+
+# String Expressions
+"Hola" + " " + "Mundo"
+
+# Function Call Expressions
+len("Python")
