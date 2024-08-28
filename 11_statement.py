8eac2cae138bff004dbcc1db707be2880b710932
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-08-28 01:17:48 -0600

    feat: Add explanation for statement in Python

---
+++ 11_statement.py
@@ -0,0 +1,23 @@
+"""
+Statement
+A statement in Python is a logical instruction which Python interpreter can read and execute.
+"""
+# Assignment statement
+x = 10
+
+# Flow control statement
+if x > 5:
+    print("Mayor que 5")
+
+# Loop statement
+for i in range(3):
+    print(i)
+
+# Difference between statement and expression
+# Statement
+x = 10
+
+# Expression
+print(10 + 20) # 30
+print(1 > 2) # False
+print("Hola" + " mundo.") # "Hola mundo."
