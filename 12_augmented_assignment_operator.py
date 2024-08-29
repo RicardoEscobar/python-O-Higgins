0cab13baa560d900caff91a321d5c7fef6d89250
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-08-29 09:16:43 -0600

    feat: Add augmented assignment operator examples in Python

---
+++ 12_augmented_assignment_operator.py
@@ -0,0 +1,20 @@
+"""
+Augmented Assignment Operator.
+Is a shorthand way of assigning a value to a variable based on the variable's current value.
+"""
+a_value = 5
+a_value -= 2
+print(a_value) # 7
+
+# Examples of all the operators
+x = 5
+x *= 3
+x /= 3
+x %= 3
+x //= 3
+x **= 3
+x &= 3
+x |= 3
+x ^= 3
+x >>= 3
+x <<= 3
