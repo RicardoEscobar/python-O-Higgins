38dae0e48f02dfad0dbdeb6109fa7fcb123b4202
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-09-11 18:10:14 -0600

    Add built-in functions and methods examples in Python

---
+++ 17_built_in_functions_methods.py
@@ -0,0 +1,11 @@
+"""
+Built-in Functions and Methods
+Are functions that are built into Python that can be used to perform operations on data.
+Methods are functions that are associated with objects.
+"""
+
+print("Built-in Functions and Methods")
+texto = "hola mundo"
+print(texto.upper())
+print(texto.capitalize())
+print(texto.replace("mundo", "Python"))
