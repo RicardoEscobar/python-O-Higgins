1a7371d9de0b3927e778f5144a63d75b7a07f18e
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-09-12 22:43:55 -0600

    Add bool examples and explanations in Python

---
+++ 18_bool.py
@@ -0,0 +1,32 @@
+"""
+Bool
+Is a data type that is either True or False.
+"""
+# a = 2
+# b = 3
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+
+# Implicit conversion to bool examples
+# print(bool(0)) # False
+# print(bool(1)) # True
+# print(bool(2)) # True
+# print(bool('')) # False
+# print(bool(' ')) # True
+# print(bool('hello')) # True
+# print(bool([])) # False
+# print(bool([1, 2, 3])) # True
+# print(bool({})) # False
+# print(bool({1: 'a', 2: 'b'})) # True
+# print(bool(None)) # False
+
+# What is a bool in Python?
+
+# Compare
+5 > 3
+
+# Variables
+a = True
+b = False
