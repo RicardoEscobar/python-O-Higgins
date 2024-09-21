36d33ae0c479c6801cb4260621d85d77accafc72
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-09-20 18:58:44 -0600

    Refactor list index example and update list items in Python

--- 20_list_index.py
+++ 20_list_index.py
@@ -2,5 +2,25 @@
 List Index
 The index is used to access or refer to an element in a list.
 """
+
+# Positive indexing
 # indexes: 0  1  2  3  4
 numbers = [1, 2, 3, 4, 5]
+
+# Negative indexing
+#           -3         -2         -1
+lista = ['manzana', 'banana', 'cereza']
+print(lista[-1])
+print(lista[-2])
+
+# Index error
+#           0          1         2          3?
+lista = ['manzana', 'banana', 'cereza']
+# print(lista[3])
+
+# Update list items
+lista = ['manzana', 'banana', 'cereza']
+#           0           1         2
+print(lista)
+lista[1] = 'naranja'
+print(lista)
