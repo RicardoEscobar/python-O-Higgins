69ec79c106b5621a08cfae903d5a3a7e561dbe4a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-10-02 19:48:35 -0600

    Add list range examples in Python

---
+++ 24_list_range.py
@@ -0,0 +1,17 @@
+"""
+List Patterns
+Common patterns when working with lists.
+"""
+
+# Create a list with range
+lista = list(range(5))
+
+# Specify a range with start and stop
+lista = list(range(1,5))
+
+# Specify a range with step
+lista = list(range(0, 10, 2))
+
+# Specify a range with negative step
+lista = list(range(10, 0, -1))
+print(lista)
